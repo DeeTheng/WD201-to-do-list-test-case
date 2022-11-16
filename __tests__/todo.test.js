1eda81b80837a468f4454ec8476061681ecea85c
Dipti Theng <deepti.theng@gmail.com>
2022-11-17 01:08:46 +0530

    fix date issue

--- __tests__/todo.test.js
+++ __tests__/todo.test.js
@@ -1,19 +1,16 @@
 const todoList = require("../todo");
-let today = new Date().toLocaleDateString();
+let today = new Date().toLocaleDateString("en-CA");
 
 const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("Test Todo List Function: ", () => {
   beforeAll(() => {
-    add({ title: "Submit assignment", dueDate: yesterday, completed: false });
-    add({ title: "Pay rent", dueDate: today, completed: true });
-    add({ title: "Service Vehicle", dueDate: today, completed: false });
-    add({ title: "File taxes", dueDate: tomorrow, completed: false });
+    add({ title: "Submit assignment", dueDate: new Date().toLocaleDateString("en-CA"), completed: false });
   });
 
   test("Test add Method: ", () => {
     let length = all.length;
-    add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
+    add({ title: "Pay electric bill", dueDate: new Date().toLocaleDateString("en-CA"), completed: false });
     expect(all.length).toBe(length + 1);
   });
 
